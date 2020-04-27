0600df1798e8f113aab9793f215e5b2aa992d094
Dapacr <davidcruz72@gmail.com>
2020-04-27 15:21:31 -0700

    dev

--- dev.js
+++ dev.js
@@ -1,18 +1,19 @@
+const fetch = require('node-fetch');
 const sonarr = require('./sonarr.js');
 
-sonarr.getInfo();
+// sonarr.getInfo();
 
-// fetch(`${sonarrURL}/api/series/?apikey=${sonarrToken}`)
-// 	.then((res) => {
-// 		return res.json();
-// 	})
-// 	.then((json) => {
-// 		json.forEach((entry) => {
-// 			Object.entries(entry).forEach(([ k, v ]) => {
-// 				console.log(`${k}: ${v}`);
-// 			});
-// 		});
-// 	})
-// 	.catch((err) => {`
-// 		console.error(err);
-// 	});
+fetch(`${sonarrURL}/api/series/?apikey=${sonarrToken}`)
+	.then((res) => {
+		return res.json();
+	})
+	.then((json) => {
+		json.forEach((entry) => {
+			Object.entries(entry).forEach(([ k, v ]) => {
+				console.log(`${k}: ${v}`);
+			});
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+	});
